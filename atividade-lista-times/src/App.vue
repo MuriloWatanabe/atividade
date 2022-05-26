74f378c621bd5071664ad873ee54d7c75a313ce1
MuriloWatanabe <99224795+MuriloWatanabe@users.noreply.github.com>
2022-05-26 11:47:37 -0300

    Add files via upload

---
+++ atividade-lista-times/src/App.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <div class="Home">
+      <RouterLink to="/">Home</RouterLink>
+    </div>
+    <div class="Times">
+      <RouterLink to="/Time">Time</RouterLink>
+    </div>
+    <div class="Jogadores">
+      <RouterLink to="/about">Jogadores</RouterLink>
+    </div>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  <footer>rodape</footer>
+</template>
+<style>
+header {
+  border: 1px black solid;
+  height: 15%;
+  padding: 30px;
+}
+footer {
+  border: 1px black solid;
+}
+main {
+  height: 150vh;
+}
+.Jogadores {
+  float: left;
+  padding-left: 100px;
+  padding-bottom: 0px;
+}
+.Times {
+  float: left;
+  padding-left: 70px;
+}
+.Home{
+  float: left;
+}
+</style>
